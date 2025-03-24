6a709e8a9dac65be2f0dd52407c02a137256f823
brn342 <bruno.ocampo@correo.ucu.edu.uy>
2025-03-24 10:02:04 -0300

    Revert "Contador vocales"

--- ContadorPalabras.java
+++ ContadorPalabras.java
@@ -2,62 +2,33 @@ import java.util.List;
 
 public class ContadorPalabras {
 
-    public int[] contarVocales(String frase) {
-        int contadorConsonantes = 0;
-        int contadorVocales = 0;
+    public int contarPalabras(String frase) {
+        boolean enPalabra = false;
+
+        int contador = 0;
 
         for (int i = 0; i < frase.length(); i++) {
-            char letra = Character.toLowerCase(frase.charAt(i));
-            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
-                contadorVocales++;
-            } else if (Character.isLetter(letra)){
-                contadorConsonantes++;
-            }
-        }
-        return new int[]{contadorVocales, contadorConsonantes};
-    }
+            char c = frase.charAt(i);
 
-    public int contarPalabras(String palabras) {
-
-        int contadorPalabras = 0;
-        boolean recorriendoPalabra = false;
-        boolean hayLetra = false;
-
-        for (int i = 0; i < palabras.length(); i++) {
-
-            char letra = palabras.charAt(i);
-
-            if (Character.isLetterOrDigit(letra)) {
-                if (Character.isLetter(letra)) {
-                    hayLetra = true;
-                }
-                if (!recorriendoPalabra) {
-                    recorriendoPalabra = true;
+            if (Character.isLetter(c)) {
+                if (!enPalabra) {
+                    contador++;
+                    enPalabra = true;
                 }
             } else {
-                if (recorriendoPalabra && hayLetra) {
-                    contadorPalabras++;
-                }
-                recorriendoPalabra = false;
-                hayLetra = false;
+                enPalabra = false;
             }
         }
+        return contador-1;
 
-        if (recorriendoPalabra && hayLetra) {
-            contadorPalabras++;
-        }
-
-        return contadorPalabras;
     }
 
 }
 class Principal {
 
     public static void main(String[] args) {
-        String palabras = "Hola!Mundo 123";
+        String palabras = "kjlaehfhujoaef aefioajeoifjioaefoiaonae oaenf1faiopf dwdwdw wcwdwd f 67git fnfffw efefe f f f";
         ContadorPalabras contador = new ContadorPalabras();
-
-        System.out.println("Vocales en la frase: " + contador.contarVocales(palabras)[0] + ". Consonantes: " + contador.contarVocales(palabras)[1]);
         System.out.println(contador.contarPalabras(palabras));
     }
 }
